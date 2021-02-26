1a3f73cedaeba81813e4c532ee7ce87aa87a4f69
Jonathan Saunders <jonnysaunders10@gmail.com>
2021-02-26 08:14:43 -0500

    forms and button toggle

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
-function App() {
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -26,6 +29,13 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -40,7 +50,10 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header 
+      onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
       {deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show'}
     </div>
